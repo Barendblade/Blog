0e8be521edac31a0913b4673bec860b4bcfaf8a0
Barendblade <chessplayer345man@gmail.com>
2026-01-12 05:53:27 +0200

    Update glossary.js

--- js/glossary.js
+++ js/glossary.js
@@ -8,5 +8,15 @@ const glossaryTerms = [
         id: "bias",
         term: "Bias",
         definition: "a tendency to favor or lean toward a particular perspective, idea, or outcome, often unfairly or unconsciously."
+    },
+    {
+        id: "verdant",
+        term: "Verdant",
+        definition: "of the bright green colour of lush grass."
+    },
+    {
+        id: "epiphany",
+        term: "Epiphany",
+        definition: "a moment of sudden and great revelation or realization."
     }
 ]
